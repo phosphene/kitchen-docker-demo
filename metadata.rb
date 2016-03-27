9b046e24533d43a9df182a5b6001dc96c37ea219
ed phillips <ed@cronos.net>
2016-03-27 14:18:33 -0700

    metadata and recipes

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'kitchen-docker-demo'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures kitchen-docker-demo'
+long_description 'Installs/Configures kitchen-docker-demo'
+version          '0.1.0'
