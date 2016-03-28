3e426344fcfdcbd4307aad4792507485de5ee078
ed phillips <ed@cronos.net>
2016-03-28 11:12:44 -0700

    depends golang

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures kitchen-docker-demo'
 long_description 'Installs/Configures kitchen-docker-demo'
 version          '0.1.0'
+
+depends "golang"
